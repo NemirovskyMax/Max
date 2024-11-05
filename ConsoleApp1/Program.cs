8cd5d9183ba1d3dd3c08c7057d0b553600d98b32
NemirovskyMax <artemjaik@gmail.com>
2024-11-05 10:45:42 +0300

    Add
    Task

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Text1
+            //Console.Write("Maxim");
+            //string name = Console.ReadLine();
+            //Console.WriteLine($"Maxim:{name}");
+            //Text2
+            //Console.Write("введите ваше имя");
+            //string name = Console.ReadLine();
+            //Console.WriteLine($"Привет:{name}");]
+            //Text3
+            //Console.Write("незвание футбольной командый");
+            //string name = Console.ReadLine();
+            //Console.WriteLine($"{name}- это чемпион");
+            //Text4
+            //Console.Write("как вас зовут?");
+            //string name = Console.ReadLine();
+            //Console.Write("какая у вас фамилия?");
+            //string familia = Console.ReadLine();
+            //Console.WriteLine($"{name}\t{familia}");
+            //Text8
+            //Console.Write("Здравствуй, я компьютер, а тебя как зовут? Коля");
+            //string tex = Console.ReadLine();
+            //Console.Write("Очень приятно, Коля.");
+            //string tex2 = Console.ReadLine();
+            //Console.Write("Сколько тебе лет? 16");
+            //string tex3 = Console.ReadLine();
+            //Console.Write("Ого! Целых 16 лет! Ты уже совсем взрослый!");
+            //string tex4 = Console.ReadLine();
+            //Console.WriteLine($"{tex}\t{tex2}{tex3}{tex4}");
+            //Text6
+            //Console.Write("Введите название футбольной команды");
+            //string name = Console.ReadLine();
+            //Console.Write($"Колличество символов:{name.Length}");
+            //Text10
+            //Console.Write("Как тебя зовут?");
+            //string name = Console.ReadLine();
+            //Console.Write("Сколько тебе лет?");
+            //int age = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"{name},Ты родился в {2024 - age}");
+             
+
+            
+
+
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
